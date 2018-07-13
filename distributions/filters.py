a5f5d64c4cddbe45645d9df9918278680fb06ad1
PhilipConte <philipmconte@gmail.com>
2018-07-13 13:48:04 -0400

    added more filters

--- distributions/filters.py
+++ distributions/filters.py
@@ -6,5 +6,12 @@ class SectionFilter(django_filters.FilterSet):
         model = Section
         fields = {
             'course__department': ['icontains'],
+            'course__number': ['contains'],
             'course__title': ['icontains'],
+            'course__hours': ['exact'],
+            'instructor': ['icontains'],
+            'CRN': ['contains'],
+            'average_GPA': ['exact', 'lt', 'gt'],
+            'term__semester': ['iexact'],
+            'term__year': ['exact'],
         }
