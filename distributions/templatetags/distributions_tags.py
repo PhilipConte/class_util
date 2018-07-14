f8c853e02084080b593ef99a305f0681e4de6453
PhilipConte <philipmconte@gmail.com>
2018-07-14 07:52:49 -0400

    moved stats calculation to utils module

--- distributions/templatetags/distributions_tags.py
+++ distributions/templatetags/distributions_tags.py
@@ -1,16 +1,11 @@
 from django.template.loader import get_template
 from django import template
 
+from distributions.utils import sections_stats
+
 register = template.Library()
 
 def average_sections(sections, title):
-    students = sum([s.class_size for s in sections])
-    gpa = float(sum([s.average_GPA * s.class_size / students for s in sections]))
-    a = float(sum([s.As * s.class_size / students for s in sections]))
-    b = float(sum([s.Bs * s.class_size / students for s in sections]))
-    c = float(sum([s.Cs * s.class_size / students for s in sections]))
-    d = float(sum([s.Ds * s.class_size / students for s in sections]))
-    f = float(sum([s.Fs * s.class_size / students for s in sections]))
-    return {'title': title + ' Distribution', 'stats': {'gpa': gpa, 'a': a, 'b': b, 'c': c, 'd': d, 'f': f}}
+    return {'title': title + ' Distribution', 'stats': sections_stats(sections)}
 
 register.inclusion_tag(get_template('tags/average_sections.html'))(average_sections)
--- distributions/utils.py
+++ distributions/utils.py
@@ -2,3 +2,13 @@ from django.utils.html import format_html
 
 def gen_link(value, link):
     return format_html('<a href={}>{}</a>', link, value)
+
+def sections_stats(sections):
+    students = sum([s.class_size for s in sections])
+    gpa = float(sum([s.average_GPA * s.class_size / students for s in sections]))
+    a = float(sum([s.As * s.class_size / students for s in sections]))
+    b = float(sum([s.Bs * s.class_size / students for s in sections]))
+    c = float(sum([s.Cs * s.class_size / students for s in sections]))
+    d = float(sum([s.Ds * s.class_size / students for s in sections]))
+    f = float(sum([s.Fs * s.class_size / students for s in sections]))
+    return {'gpa': gpa, 'a': a, 'b': b, 'c': c, 'd': d, 'f': f}
