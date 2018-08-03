df4e1e82bfb8d6b769542b702370a6f163335b58
PhilipConte <philipmconte@gmail.com>
2018-08-03 07:27:48 -0400

    improved paths of filtered vs un-filtered

--- distributions/urls.py
+++ distributions/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='courses/search/', permanent=False), name='index'),
-    path('sections/filtered/', views.SectionFilteredListView.as_view(), name='section_filtered_list'),
-    path('courses/', views.CourseListView.as_view(), name='course_list'),
+    path('sections/', views.SectionFilteredListView.as_view(), name='section_filtered_list'),
+    path('courses/home/', views.CourseListView.as_view(), name='course_list'),
     path('courses/search/', views.CourseSearchView.as_view(), name = 'course_search'),
-    path('courses/filtered/', views.CourseFilteredListView.as_view(), name = 'course_filtered_list'),
+    path('courses/', views.CourseFilteredListView.as_view(), name = 'course_filtered_list'),
     path('courses/department=<str:department>+number=<int:number>+title=<str:title>+hours=<int:hours>/', views.CourseDetailView.as_view(), name='course_detail'),
     path('courses/department=<str:department>+number=<int:number>+title=<str:title>+hours=<int:hours>/<str:instructor>/', views.CourseInstructorDetailView.as_view(), name='course_instructor_detail'),
 ]
