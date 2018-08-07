e2b5d2d477e727da91e5a03b997c308320d0b758
PhilipConte <philipmconte@gmail.com>
2018-08-07 08:34:11 -0400

    preparing for course search redirecting to page

--- distributions/views.py
+++ distributions/views.py
@@ -47,6 +47,14 @@ class CourseFilteredListView(FilteredSingleTableView):
         context['title'] = 'Filtered Courses'
         return context
 
+    # def get(self, request):
+    #     qs = self.get_table_data()
+    #     if qs.count() == 1:
+    #         print('1 item')
+    #         return HttpResponseRedirect(qs.first().get_absolute_url()) 
+    #     else:
+    #         return super().get(request)
+
 class CourseListView(CourseFilteredListView):
     filter_class = CourseFilterMulti
     template_name = 'course_list.html'
