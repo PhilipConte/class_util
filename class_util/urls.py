a17464788adefb0aa482eed020ed85106100bf24
PhilipConte <philipmconte@gmail.com>
2018-08-10 06:43:15 -0400

    moved doughnutlabel plugin to static file

--- class_util/urls.py
+++ class_util/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='distributions', permanent=False), name='index'),
     path('distributions/', include('distributions.urls', namespace='distributions')),
     path('api/distributions/', include('distributions.api.urls', namespace='distributions-api'))
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
