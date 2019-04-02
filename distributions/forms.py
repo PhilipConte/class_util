7a213b249f09f955130e419319307fa1ada10fdf
PhilipConte <philipmconte@gmail.com>
2019-04-02 15:15:41 -0400

    improved form rendering

--- distributions/forms.py
+++ distributions/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 
 css_classes = 'form-control bulk-input'
-styles = ''
 
 class CSSForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.keys():
-            self.fields[field].widget.attrs.update({'class': css_classes, 'style': styles})
+            self.fields[field].widget.attrs.update({'class': css_classes})
