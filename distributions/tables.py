f1befc470532df613fd61d35f46983e928d23f5b
PhilipConte <philip@pconte.me>
2020-03-12 13:07:32 -0400

    variety of simplifications

--- distributions/tables.py
+++ distributions/tables.py
@@ -1,4 +1,3 @@
-from urllib.parse import quote
 from django.utils.html import format_html
 from django.urls import reverse
 import django_tables2 as tables
@@ -6,13 +5,6 @@ from . import models as m
 
 GRADES = ['average_GPA', 'As', 'Bs', 'Cs', 'Ds', 'Fs']
 
-class RoundColumn(tables.Column):
-    def __init__(self, decimals=1, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.decimals = decimals
-
-    def render(self, value):
-        return round(value, self.decimals)
 
 class CourseTable(tables.Table):
     class Meta:
@@ -20,41 +12,26 @@ class CourseTable(tables.Table):
         fields = ['department', 'number', 'title', 'hours', *GRADES]
     
     title = tables.Column(linkify=True)
-    average_GPA = RoundColumn(2)
-    As = RoundColumn(verbose_name='A%')
-    Bs = RoundColumn(verbose_name='B%')
-    Cs = RoundColumn(verbose_name='C%')
-    Ds = RoundColumn(verbose_name='D%')
-    Fs = RoundColumn(verbose_name='F%')
+
 
 class SectionTable(tables.Table):
     class Meta:
         model = m.Section
-        fields= ['term', 'course', 'CRN', 'instructor', *GRADES, 'withdrawals', 'class_size']
+        fields= ['term', 'CRN', *GRADES, 'withdrawals', 'class_size']
 
-    course = tables.Column(linkify=True)
-    instructor = tables.Column(linkify=True)
 
 class GroupedSectionTable(tables.Table):
     def __init__(self, *args, **kwargs):
-        temp_course_args = kwargs.pop("course_args")
+        slug = kwargs.pop("slug")
         super().__init__(*args, **kwargs)
-        self.course_args = temp_course_args
+        self.slug = slug
     
     class Meta:
         model = m.Section
         fields = ['instructor', 'sections_taught', 'withdrawals', *GRADES]
-    
-    instructor = tables.Column()
-    average_GPA = RoundColumn(2)
-    As = RoundColumn(verbose_name='A%')
-    Bs = RoundColumn(verbose_name='B%')
-    Cs = RoundColumn(verbose_name='C%')
-    Ds = RoundColumn(verbose_name='D%')
-    Fs = RoundColumn(verbose_name='F%')
 
     def render_instructor(self, value):
         return format_html('<a href={}>{}</a>',
             reverse('distributions:course_instructor_detail', args=[
-                *self.course_args, quote(value, safe='')]),
+                self.slug, value]),
             value)
--- distributions/views.py
+++ distributions/views.py
@@ -13,7 +13,7 @@ class FilteredSingleTableView(django_tables2.SingleTableView):
     filter_class = None
 
     def get_table_data(self):
-        data = super().get_table_data()
+        data = self.get_queryset()
         self.filter = self.filter_class(self.request.GET, queryset=data)
         return self.filter.qs
 
@@ -29,14 +29,6 @@ class CourseFilteredListView(FilteredSingleTableView):
     filter_class = CourseFilter
     template_name ='course_list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['hierarchy'] = [
-            {'text': 'Courses', 'link': reverse('distributions:course_list')},
-            {'text': 'Filtered'},
-        ]
-        return context
-
     def get(self, request):
         qs = self.get_table_data()
         if qs.count() == 1:
@@ -44,25 +36,15 @@ class CourseFilteredListView(FilteredSingleTableView):
         else:
             return super().get(request)
 
+
 class CourseListView(CourseFilteredListView):
     filter_class = CourseFilterMulti
     template_name = 'course_list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['hierarchy'] = [{'text': 'Courses'}]
-        return context
 
 class CourseSearchView(TemplateView):
     template_name = 'course_search.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['hierarchy'] = [
-            {'text': 'Courses', 'link': reverse('distributions:course_list')},
-            {'text': 'Search'},
-        ]
-        return context
 
 class CourseDetailView(FilteredSingleTableView):
     model = Section
@@ -70,32 +52,19 @@ class CourseDetailView(FilteredSingleTableView):
     filter_class = GroupedSectionFilter
     template_name = 'course_detail.html'    
 
-    def parse_params(self):
-        self.course = get_object_or_404(Course,
-            slug=self.kwargs['slug'])
-        self.sections = self.course.sections.all()
-
-    def get_table_data(self):
-        data = self.sections.group_by_instructor()
-        self.filter = self.filter_class(self.request.GET, queryset=data)
-        return self.filter.qs
+    def get_queryset(self):
+        self.course = get_object_or_404(Course, slug=self.kwargs['slug'])
+        return self.course.sections.group_by_instructor()
 
     def get_table_kwargs(self):
         return {
             'request': self.request,
-            'course_args': [self.course.slug]}
+            'slug': self.course.slug}
 
     def get_context_data(self, **kwargs):
-        self.parse_params()
-
         context = super().get_context_data(**kwargs)
-        context['hierarchy'] = [
-            {'text': 'Courses', 'link': reverse('distributions:course_list')},
-            {'text': self.course.short()},
-        ]
         context['title'] = self.course.html()
         context['course'] =  self.course
-        context['sections'] =  self.sections
         return context
 
 class CourseInstructorDetailView(django_tables2.SingleTableView):
@@ -103,35 +72,15 @@ class CourseInstructorDetailView(django_tables2.SingleTableView):
     table_class = SectionTable
     template_name = 'course_instructor_detail.html'    
 
-    def parse_params(self):
-        self.course = get_object_or_404(Course,
-            slug=self.kwargs['slug'])
-        self.course_sections = self.course.sections.all()
+    def get_queryset(self):
+        self.course = get_object_or_404(Course, slug=self.kwargs['slug'])
         self.instructor = unquote(self.kwargs['instructor'])
-        self.sections = self.course_sections.filter(
-            instructor=self.instructor)
-
-    def get_table_data(self):
-        return self.sections
-
-    def get_table_kwargs(self):
-        return {
-            'request': self.request,
-            'exclude': ['id', 'course', 'instructor', 'slug']}
+        return self.course.sections.filter(instructor=self.instructor)
 
     def get_context_data(self, **kwargs):
-        self.parse_params()
-
         context = super().get_context_data(**kwargs)
-        context['hierarchy'] = [
-            {'text': 'Courses', 'link': reverse('distributions:course_list')},
-            {'text': self.course.short(), 'link': self.course.get_absolute_url()},
-            {'text': self.instructor},
-        ]
         context['title'] = '<strong>' + self.course.short() \
             + '</strong>: Statistics for Professor ' + self.instructor
         context['course'] =  self.course
-        context['course_sections'] = self.course_sections
         context['instructor'] = self.instructor
-        context['sections'] =  self.sections
         return context
